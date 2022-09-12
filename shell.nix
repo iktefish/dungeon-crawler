a1656ca25f57b8f4c30b50dd46c539b866a003a9
iktefish <87042742+iktefish@users.noreply.github.com>
2022-09-12 18:23:57 +0600

    Changes to shell.nix

--- shell.nix
+++ shell.nix
@@ -1,17 +1,6 @@
-with import <nixpkgs> { };
-let
-  unstableTarball =
-    fetchTarball
-      https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz;
-  pkgsUnstable = import unstableTarball { };
-
-in
-stdenv.mkDerivation {
-  name = "rust";
-  buildInputs = with pkgsUnstable; [
-    rustc
-    cargo
-    rust-analyzer
-    rustfmt
-  ];
-}
+with import <nixpkgs> {};
+  stdenv.mkDerivation {
+    name = "rust";
+    buildInputs = with pkgs; [
+    ];
+  }
